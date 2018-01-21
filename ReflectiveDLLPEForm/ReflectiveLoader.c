1c7822d33f89bc5ba55498a9f0b461af03c8e0e8
SudoZhange <ludianjun1996@gmail.com>
2018-01-21 15:32:33 +0800

    Finished Demo

--- ReflectiveDLLPEForm/ReflectiveLoader.c
+++ ReflectiveDLLPEForm/ReflectiveLoader.c
@@ -1,4 +1,4 @@
-//===============================================================================================//
+﻿//===============================================================================================//
 // Copyright (c) 2012, Stephen Fewer of Harmony Security (www.harmonysecurity.com)
 // All rights reserved.
 // 
@@ -44,7 +44,8 @@ HINSTANCE hAppInstance = NULL;
 //         otherwise it is assumed you are calling the ReflectiveLoader via a stub.
 
 // This is our position independent reflective DLL loader/injector
-
+//写在前面：该函数实质上就是实现LoadLibrary的功能，如果我们用原项目本身，由于没有使用LoadLibraryA函数，所以调试起来比较麻烦，
+//于是，我将它做成可执行格式的，功能相同，都是将缓冲区中的dll解析，实现加载的功能。
 ULONG_PTR WINAPI ReflectiveLoader(ULONG_PTR callAddress)
 {
 	// the functions we need
@@ -78,49 +79,57 @@ ULONG_PTR WINAPI ReflectiveLoader(ULONG_PTR callAddress)
 	// STEP 0: calculate our images current base address
 
 	// we will start searching backwards from our callers return address.
-	//
+	//callAddress：在缓冲区开辟空间的起始地址，在原注入中uiLibraryAddress = caller();
+	//caller定义：__declspec(noinline) ULONG_PTR caller( VOID ) { return (ULONG_PTR)_ReturnAddress(); }
+	//caller功能：获取当前指令的下一条指令的地址。
+	//0x10偏移是为了复现找文件起始地址的情况，不至于开始就是0x4D5A
 	uiLibraryAddress = callAddress + 0x10;
 
 	// loop through memory backwards searching for our images base address
 	// we dont need SEH style search as we shouldnt generate any access violations with this
+	//通过遍历内存地址，找到文件在内存中的起始地址
 	while (TRUE)
 	{
 		if (((PIMAGE_DOS_HEADER)uiLibraryAddress)->e_magic == IMAGE_DOS_SIGNATURE)
 		{
+			//pe头偏移RVA
 			uiHeaderValue = ((PIMAGE_DOS_HEADER)uiLibraryAddress)->e_lfanew;
 			// some x64 dll's can trigger a bogus signature (IMAGE_DOS_SIGNATURE == 'POP r10'),
 			// we sanity check the e_lfanew with an upper threshold value of 1024 to avoid problems.
+			//判断PE头的正确性
 			if (uiHeaderValue >= sizeof(IMAGE_DOS_HEADER) && uiHeaderValue < 1024)
 			{
+				//pe头在内存中的位置
 				uiHeaderValue += uiLibraryAddress;
 				// break if we have found a valid MZ/PE header
+				//如果找到文件头就退出循环
 				if (((PIMAGE_NT_HEADERS)uiHeaderValue)->Signature == IMAGE_NT_SIGNATURE)
 					break;
 			}
 		}
 		uiLibraryAddress--;
 	}
-
+	//处理我们loader需要的内核导出表
 	// STEP 1: process the kernels exports for the functions our loader needs...
-
+	//获得PEB,64位下，GS指向段内存的0x60偏移处就是peb结构的位置，32位下位FS:[0x30]
 	// get the Process Enviroment Block
 	uiBaseAddress = __readgsqword(0x60);//uiBaseAddress -> peb
-
-
-										// get the processes loaded modules. ref: http://msdn.microsoft.com/en-us/library/aa813708(VS.85).aspx
+	//获取进程加载模块
+	// get the processes loaded modules. ref: http://msdn.microsoft.com/en-us/library/aa813708(VS.85).aspx
 	uiBaseAddress = (ULONG_PTR)((_PPEB)uiBaseAddress)->pLdr;//uiBaseAddress->pLdr
-
-															// get the first entry of the InMemoryOrder module list
-	uiValueA = (ULONG_PTR)((PPEB_LDR_DATA)uiBaseAddress)->InMemoryOrderModuleList.Flink;
+	//获取第一个“内存顺序”模块列表入口
+	// get the first entry of the InMemoryOrder module list
+	uiValueA = (ULONG_PTR)((PPEB_LDR_DATA)uiBaseAddress)->InMemoryOrderModuleList.Flink;//uiBaseAddress->PLDR_DATA_TABLE_ENTRY
 	while (uiValueA)
 	{
 		// get pointer to current modules name (unicode string)
+		//当前模块名地址
 		uiValueB = (ULONG_PTR)((PLDR_DATA_TABLE_ENTRY)uiValueA)->BaseDllName.pBuffer;
 		// set bCounter to the length for the loop
 		usCounter = ((PLDR_DATA_TABLE_ENTRY)uiValueA)->BaseDllName.Length;
 		// clear uiValueC which will store the hash of the module name
 		uiValueC = 0;
-
+		//计算模块名的hash
 		// compute the hash of the module name...
 		do
 		{
@@ -249,11 +258,12 @@ ULONG_PTR WINAPI ReflectiveLoader(ULONG_PTR callAddress)
 
 		// get the next entry
 		uiValueA = DEREF(uiValueA);
-	}
+	}//end while
 
 	// STEP 2: load our image into a new permanent location in memory...
 
 	// get the VA of the NT Header for the PE to be loaded
+	//NT头的虚拟地址，原DLL文件中
 	uiHeaderValue = uiLibraryAddress + ((PIMAGE_DOS_HEADER)uiLibraryAddress)->e_lfanew;
 
 	// allocate all the memory for the DLL to be loaded into. we can load at any address because we will  
@@ -261,34 +271,36 @@ ULONG_PTR WINAPI ReflectiveLoader(ULONG_PTR callAddress)
 	uiBaseAddress = (ULONG_PTR)pVirtualAlloc(NULL, ((PIMAGE_NT_HEADERS)uiHeaderValue)->OptionalHeader.SizeOfImage, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 
 	// we must now copy over the headers
-	uiValueA = ((PIMAGE_NT_HEADERS)uiHeaderValue)->OptionalHeader.SizeOfHeaders;
-	uiValueB = uiLibraryAddress;
-	uiValueC = uiBaseAddress;
-
+	uiValueA = ((PIMAGE_NT_HEADERS)uiHeaderValue)->OptionalHeader.SizeOfHeaders;//所有头+节表的大小
+	uiValueB = uiLibraryAddress;//DLL的起始地址，即缓冲区的起始地址
+	uiValueC = uiBaseAddress;//dll将被加载的地址的起始地址
+	//复制头和节表的数据到新开辟的缓冲区
 	while (uiValueA--)
 		*(BYTE *)uiValueC++ = *(BYTE *)uiValueB++;
 
 	// STEP 3: load in all of our sections...
-
+	//节表的第一项
 	// uiValueA = the VA of the first section
 	uiValueA = ((ULONG_PTR)&((PIMAGE_NT_HEADERS)uiHeaderValue)->OptionalHeader + ((PIMAGE_NT_HEADERS)uiHeaderValue)->FileHeader.SizeOfOptionalHeader);
 
 	// itterate through all sections, loading them into memory.
+	//pe中节的数量
 	uiValueE = ((PIMAGE_NT_HEADERS)uiHeaderValue)->FileHeader.NumberOfSections;
 	while (uiValueE--)
 	{
+		//节的虚拟地址
 		// uiValueB is the VA for this section
 		uiValueB = (uiBaseAddress + ((PIMAGE_SECTION_HEADER)uiValueA)->VirtualAddress);
-
+		//节的文件偏移
 		// uiValueC if the VA for this sections data
 		uiValueC = (uiLibraryAddress + ((PIMAGE_SECTION_HEADER)uiValueA)->PointerToRawData);
-
+		//节的大小
 		// copy the section over
 		uiValueD = ((PIMAGE_SECTION_HEADER)uiValueA)->SizeOfRawData;
-
+		//拷贝数据
 		while (uiValueD--)
 			*(BYTE *)uiValueB++ = *(BYTE *)uiValueC++;
-
+		//下一个节
 		// get the VA of the next section
 		uiValueA += sizeof(IMAGE_SECTION_HEADER);
 	}
@@ -300,18 +312,23 @@ ULONG_PTR WINAPI ReflectiveLoader(ULONG_PTR callAddress)
 
 	// we assume their is an import table to process
 	// uiValueC is the first entry in the import table
+	//基地址+RVA即导入表描述符的地址VA
 	uiValueC = (uiBaseAddress + ((PIMAGE_DATA_DIRECTORY)uiValueB)->VirtualAddress);
 
 	// itterate through all imports
+	//链接库名字
 	while (((PIMAGE_IMPORT_DESCRIPTOR)uiValueC)->Name)
 	{
 		// use LoadLibraryA to load the imported module into memory
+		//使用LoadLibraryA将需要的模块加载到内存
 		uiLibraryAddress = (ULONG_PTR)pLoadLibraryA((LPCSTR)(uiBaseAddress + ((PIMAGE_IMPORT_DESCRIPTOR)uiValueC)->Name));
 
 		// uiValueD = VA of the OriginalFirstThunk
+		//指向INT的IMAGE_THUNK_DATA的VA
 		uiValueD = (uiBaseAddress + ((PIMAGE_IMPORT_DESCRIPTOR)uiValueC)->OriginalFirstThunk);
 
 		// uiValueA = VA of the IAT (via first thunk not origionalfirstthunk)
+		//要导入IAT的IMAGE_THUNK_DATA结构体
 		uiValueA = (uiBaseAddress + ((PIMAGE_IMPORT_DESCRIPTOR)uiValueC)->FirstThunk);
 
 		// itterate through all imported functions, importing by ordinal if no name present
@@ -348,7 +365,7 @@ ULONG_PTR WINAPI ReflectiveLoader(ULONG_PTR callAddress)
 			}
 			// get the next imported function
 			uiValueA += sizeof(ULONG_PTR);
-			if (uiValueD)
+			if (uiValueD)//INT
 				uiValueD += sizeof(ULONG_PTR);
 		}
 
@@ -357,37 +374,39 @@ ULONG_PTR WINAPI ReflectiveLoader(ULONG_PTR callAddress)
 	}
 
 	// STEP 5: process all of our images relocations...
-
+	//实际装载和建议装载的偏移，原重定位表中的值是以程序建议的装载地址为基址
 	// calculate the base address delta and perform relocations (even if we load at desired image base)
-	uiLibraryAddress = uiBaseAddress - ((PIMAGE_NT_HEADERS)uiHeaderValue)->OptionalHeader.ImageBase;
+	uiLibraryAddress = uiBaseAddress - ((PIMAGE_NT_HEADERS)uiHeaderValue)->OptionalHeader.ImageBase;//程序建议的装载地址
 
 	// uiValueB = the address of the relocation directory
 	uiValueB = (ULONG_PTR)&((PIMAGE_NT_HEADERS)uiHeaderValue)->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC];
 
 	// check if their are any relocations present
-	if (((PIMAGE_DATA_DIRECTORY)uiValueB)->Size)
+	if (((PIMAGE_DATA_DIRECTORY)uiValueB)->Size)//重定位表大小
 	{
 		// uiValueC is now the first entry (IMAGE_BASE_RELOCATION)
+		//重定位表的地址
 		uiValueC = (uiBaseAddress + ((PIMAGE_DATA_DIRECTORY)uiValueB)->VirtualAddress);
 
 		// and we itterate through all entries...
-		while (((PIMAGE_BASE_RELOCATION)uiValueC)->SizeOfBlock)
+		while (((PIMAGE_BASE_RELOCATION)uiValueC)->SizeOfBlock)//重定位块的大小
 		{
 			// uiValueA = the VA for this relocation block
-			uiValueA = (uiBaseAddress + ((PIMAGE_BASE_RELOCATION)uiValueC)->VirtualAddress);
-
+			uiValueA = (uiBaseAddress + ((PIMAGE_BASE_RELOCATION)uiValueC)->VirtualAddress);//重定位内存页的起始RVA
+			//重定位块中的项数(整个块的大小减去结构体的大小，得到重定位项的总大小，除以每个重定位项的大小)
 			// uiValueB = number of entries in this relocation block
 			uiValueB = (((PIMAGE_BASE_RELOCATION)uiValueC)->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / sizeof(IMAGE_RELOC);
-
+			//重定位块的第一项
 			// uiValueD is now the first entry in the current relocation block
 			uiValueD = uiValueC + sizeof(IMAGE_BASE_RELOCATION);
-
+			//遍历重定位项
 			// we itterate through all the entries in the current block...
 			while (uiValueB--)
 			{
 				// perform the relocation, skipping IMAGE_REL_BASED_ABSOLUTE as required.
 				// we dont use a switch statement to avoid the compiler building a jump table
 				// which would not be very position independent!
+				//重定位项的高四位代表此重定位项的类型
 				if (((PIMAGE_RELOC)uiValueD)->type == IMAGE_REL_BASED_DIR64)
 					*(ULONG_PTR *)(uiValueA + ((PIMAGE_RELOC)uiValueD)->offset) += uiLibraryAddress;
 				else if (((PIMAGE_RELOC)uiValueD)->type == IMAGE_REL_BASED_HIGHLOW)
@@ -397,11 +416,11 @@ ULONG_PTR WINAPI ReflectiveLoader(ULONG_PTR callAddress)
 					*(WORD *)(uiValueA + ((PIMAGE_RELOC)uiValueD)->offset) += HIWORD(uiLibraryAddress);
 				else if (((PIMAGE_RELOC)uiValueD)->type == IMAGE_REL_BASED_LOW)
 					*(WORD *)(uiValueA + ((PIMAGE_RELOC)uiValueD)->offset) += LOWORD(uiLibraryAddress);
-
+				//下一个重定位项
 				// get the next entry in the current relocation block
 				uiValueD += sizeof(IMAGE_RELOC);
 			}
-
+			//下一个重定位块
 			// get the next entry in the relocation directory
 			uiValueC = uiValueC + ((PIMAGE_BASE_RELOCATION)uiValueC)->SizeOfBlock;
 		}
@@ -414,7 +433,7 @@ ULONG_PTR WINAPI ReflectiveLoader(ULONG_PTR callAddress)
 
 	// We must flush the instruction cache to avoid stale code being used which was updated by our relocation processing.
 	pNtFlushInstructionCache((HANDLE)-1, NULL, 0);
-
+	((DLLMAIN)uiValueA)((HINSTANCE)uiBaseAddress, DLL_PROCESS_ATTACH, NULL);
 	// STEP 8: return our new entry point address so whatever called us can call DllMain() if needed.
 	return uiValueA;
 }
